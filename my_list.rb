744ddac42c8dab7610b1e243c7d5dcde38b6b53c
Leandro-Barretoo <officialdevmi@gmail.com>
2021-12-15 06:11:04 -0800

    Complete MyList class and fix minor syntax error

--- my_list.rb
+++ my_list.rb
@@ -1,8 +1,21 @@
 require_relative './my_enumerables'
 
 class MyList
-  include MyEnumerables
+  include MyEnum
   def initialize(*args)
     @list = args
   end
+
+  def each
+    i = 0
+    while i < @list.length
+      yield @list[i]
+      i += 1
+    end
+  end
 end
+
+
+list = MyList.new(1, 2,3 ,4)
+puts(list.all? { |e| e > 5})
+puts(list.all? { |e| e < 5})
