843df053e3b87d0a2bbe4a23232ed87dc674d3f6
Hector <vv96rca@gmail.com>
2021-12-15 19:25:53 +0530

    Update linting errors

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -1,24 +1,23 @@
 module MyEnum
-
-    def all? 
-        each do |e|
-            return false unless yield(e)
-        end 
-        true
+  def all?
+    each do |e|
+      return false unless yield(e)
     end
+    true
+  end
 
-    def any?
-        each do |e|
-            return true if yield(e)
-        end
-        false
+  def any?
+    each do |e|
+      return true if yield(e)
     end
+    false
+  end
 
-    def filter
-        result = []
-        each do |e|
-            result << e if yield(e)
-        end
-        result
+  def filter
+    result = []
+    each do |e|
+      result << e if yield(e)
     end
+    result
+  end
 end
