44df10ba9fab6de46a2ef149cc0ba10de86d3a2c
Leandro-Barretoo <officialdevmi@gmail.com>
2021-12-15 06:12:22 -0800

    Fix rubocop offenses

--- my_list.rb
+++ my_list.rb
@@ -15,7 +15,6 @@ class MyList
   end
 end
 
-
-list = MyList.new(1, 2,3 ,4)
-puts(list.all? { |e| e > 5})
-puts(list.all? { |e| e < 5})
+list = MyList.new(1, 2, 3, 4)
+puts(list.all? { |e| e > 5 })
+puts(list.all? { |e| e < 5 })
